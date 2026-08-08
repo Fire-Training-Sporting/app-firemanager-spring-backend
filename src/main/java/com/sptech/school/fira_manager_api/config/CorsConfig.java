f2c9a8eae4134ac298c6833dd8c24ea9529c2779
Luand-Assis <luan.assis@sptech.school>
2026-08-08 01:16:46 -0300

    feat: segredos sao variaveis de ambiente local

--- src/main/java/com/sptech/school/fira_manager_api/config/CorsConfig.java
+++ src/main/java/com/sptech/school/fira_manager_api/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("*")
+                        .allowedOrigins("http://localhost:5173")
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
